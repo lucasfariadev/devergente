9585faab6d66e0e94b45748ff604a16ed7e6ff6c
lucasfariadev <lucas_faria@outlook.com>
2023-06-15 10:00:52 -0300

    primeira página de cadastro completa

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -1,9 +1,42 @@
 function irParaPagina(pagina) {
-    window.location.href = pagina;
-  }
+  window.location.href = pagina;
+}
+
+function limparValor(element) {
+  element.value = '';
+}
+
+function toggleOptions(optionsListId) {
+  var optionsListNasc = document.getElementById('optionsListNasc');
+  var optionsListGenero = document.getElementById('optionsListGenero');
   
-  function limparValor(element) {
-    element.value = '';
+  if (optionsListId === 'optionsListNasc') {
+    optionsListNasc.style.display = optionsListNasc.style.display === 'block' ? 'none' : 'block';
+    optionsListGenero.style.display = 'none';
+  } else if (optionsListId === 'optionsListGenero') {
+    optionsListGenero.style.display = optionsListGenero.style.display === 'block' ? 'none' : 'block';
+    optionsListNasc.style.display = 'none';
+  }
+}
+
+function selectOption(option) {
+  var generoElement = document.getElementById('genero');
+  var nascElement = document.getElementById('nasc');
+
+  var optionsListNasc = document.getElementById('optionsListNasc');
+  var optionsListGenero = document.getElementById('optionsListGenero');
+
+  if (optionsListNasc.style.display === 'block') {
+    nascElement.textContent = option;
+    optionsListNasc.style.display = 'none';
+  } else if (optionsListGenero.style.display === 'block') {
+    generoElement.textContent = option;
+    optionsListGenero.style.display = 'none';
   }
 
-  
+
+  if (option == "Não Binário"){
+    document.getElementById('genero').style.fontSize = "11px";
+  }
+}
+
